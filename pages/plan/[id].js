2bd5a398fcd95e9465a58ad0adecd198b7275192
Benjamin Toh <toh.benjamin123@gmail.com>
2023-01-08 15:26:11 +0800

    +

--- pages/plan/[id].js
+++ pages/plan/[id].js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 function PlanPage() {
+
+    //get trip and display current info or find a way to pull through trip info
+
+    //empty fields to add
+
+    //update trip button to mongodb
+
+    //mapbox on the side https://docs.mapbox.com/mapbox-gl-js/guides/install/
+
   return (
-    <div>[id]</div>
+    <div className='bg-white'>
+        <Header/>
+        <main className=''>
+            <h1>Trip to</h1>
+        </main>
+        <Footer/>
+    </div>
   )
 }
 
