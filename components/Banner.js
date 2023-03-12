bbe5454ed3688e83811f5388455ac5dd545a2411
Benjamin Toh <toh.benjamin123@gmail.com>
2023-03-13 03:17:24 +0800

    Edit banner font size

--- components/Banner.js
+++ components/Banner.js
@@ -15,8 +15,8 @@ function Banner() {
         className='object-cover'
       />
       <div className='absolute top-1/2 w-full text-center'>
-        <p className='text-sm sm:text-lg text-white'>Create your trip!</p>
-        <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full
+        <p className='text-lg font-bold sm:text-6xl text-white'>Create your trip!</p>
+        <button className='mt-6 text-purple-500 bg-white px-10 py-4 shadow-md rounded-full
         font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'
           onClick={()=>{router.push('/addtrip')}}
         >
