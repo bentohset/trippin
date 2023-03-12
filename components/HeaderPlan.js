0c7d538f6513b1574b473605dc71b86e170393b2
Benjamin Toh <toh.benjamin123@gmail.com>
2023-03-13 03:17:38 +0800

    Add save button for trips

--- components/HeaderPlan.js
+++ components/HeaderPlan.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { MagnifyingGlassIcon, Bars3Icon, UserCircleIcon} from '@heroicons/react/24/solid'
 import Link from 'next/link';
 
-function HeaderPlan() {
+function HeaderPlan(handleOnClick) {
   return (
     <nav className='fixed top-0 z-50 grid grid-cols-2 
     bg-white shadow-md p-2 md:px-10 w-1/2' >
@@ -19,14 +19,20 @@ function HeaderPlan() {
             </Link>
         </div>
 
-
-
-        <div className='flex items-center space-x-4 justify-end text-gray-500'>
+        <div className='flex items-center space-x-4 justify-end'>
+            <button className="text-white bg-[#FD5B61] px-6 py-2 shadow-md 
+                rounded-full font-bold hover:shadow-xl"
+                onClick={handleOnClick}
+            >
+                Save
+            </button>
+        </div>
+        {/* <div className='flex items-center space-x-4 justify-end text-gray-500'>
             <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
                 <Bars3Icon className='h-6'/>
                 <UserCircleIcon className='h-6'/>
             </div>
-        </div>
+        </div> */}
     </nav>
   )
 }
