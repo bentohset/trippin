2a66c1fd257ec2228c093c2f2c5cd234ea870dd3
Benjamin Toh <toh.benjamin123@gmail.com>
2023-06-10 15:41:19 +0800

    Edit banner

--- frontend/components/Banner.js
+++ frontend/components/Banner.js
@@ -7,17 +7,17 @@ function Banner() {
   const router = useRouter();
 
   return (
-    <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
-      <Image 
+    <div className='flex justify-center items-center h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] bg-gradient-to-tr from-[rgba(223,167,39,1)] to-[rgba(255,111,145,1)]'>
+      {/* <Image 
         src={pic}
         alt=""
         fill
         className='object-cover'
-      />
-      <div className='absolute top-1/2 w-full text-center'>
+      /> */}
+      <div className='w-full text-center'>
         <p className='text-lg font-bold sm:text-6xl text-white'>Create your trip!</p>
-        <button className='mt-6 text-[#FD5B61] bg-white px-10 py-4 shadow-md rounded-full
-        font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'
+        <button className='mt-10 text-[#FD5B61] bg-white px-10 py-4 shadow-md rounded-full
+        font-bold hover:shadow-xl active:scale-90 transition duration-150'
           onClick={()=>{router.push('/addtrip')}}
         >
           Plan New Trip
