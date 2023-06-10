ed658b85eed4c510f96801a3531c24371162165a
Benjamin Toh <toh.benjamin123@gmail.com>
2023-06-10 15:35:26 +0800

    add profile page

---
+++ frontend/components/ProfileTripCard.js
@@ -0,0 +1,164 @@
+import React, { useState, useEffect } from "react";
+import { XMarkIcon } from '@heroicons/react/20/solid'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useAuth } from "../hooks/auth";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
+
+function ProfileTripCard({ index, length, id, img, location, startDate, endDate, year, title }) {
+    const { cookies } = useAuth()
+    const [open, setOpen] = useState(false)
+    const router = useRouter()
+    console.log(startDate, length)
+
+    const convertDate = (date) => {
+		const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+		"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+		const day = date.slice(8,10) //8 and 9
+		const monthIndex = parseInt(date.slice(5,7)); //5 and 6
+		const year = date.slice(0,4)//0-3 index
+		const month = monthNames[monthIndex-1];
+		return `${day} ${month}`
+	}
+	const getYear = (date) => {
+		const year = date.slice(0,4)//0-3 index
+		return year
+	}
+
+    const handleDelete = async () => {
+        let dev = process.env.NODE_ENV !== 'production';
+    
+        const url = `${dev ? process.env.NEXT_PUBLIC_DEV_API_URL : process.env.NEXT_PUBLIC_PROD_API_URL}/user//${id}/${cookies.id}`
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            } 
+        })
+    
+        console.log(response)
+        const data = await response.json()
+        .then(data => {
+            if (response.status === 200) {
+                console.log("successfully deleted")
+                console.log(data)
+                handleClose()
+                refreshData()
+            } else {
+                console.log(data.message)
+            }
+        })
+    }
+    const handleOpen = () => {
+        setOpen(true)
+    }
+    
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+  return (
+    <div className='relative flex flex-row items-center justify-between h-full w-full'>
+      <div className={`bg-white flex w-full flex-row relative justify-between items-center cursor-pointer
+        hover:bg-gray-200 transition-transform duration-200 ease-out ${index===length-1 ? `rounded-b-xl hover:rounded-b-xl`: ``} ${index===0 ? `rounded-t-xl hover:rounded-t-xl border-b-2`: ``}`}>
+        <Link href={`/plan/${id}`} className='rounded-xl flex-1'>
+          <div className='flex flex-row items-center w-full p-2'>
+            {startDate && 
+            <div className='flex flex-col mx-2'>
+                <h2 className=''>{title}</h2>
+                
+                <h3 className='text-gray-500'>{convertDate(startDate)} - {convertDate(endDate)}</h3>
+                <h4 className='text-gray-400'>{getYear(year)}</h4>
+              
+            </div>
+            }
+          </div>
+          <div>
+
+          </div>
+        </Link>
+        
+          <button className='z-40 absolute right-1 top-1' onClick={()=>{handleOpen()}}>
+            <div className='bg-gray-300 rounded-lg flex p-1 items-center justify-center hover:bg-gray-400'>
+                <XMarkIcon
+                  className='h-4 text-gray-600 font-bold'
+                />
+            </div>
+          </button>
+        </div>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          sx={{
+            font: 'unset',
+          }}
+        >
+          <DialogTitle
+            sx={{
+              font: 'unset',
+            }}
+            style={{
+              color: 'black',
+              fontWeight: 'bold',
+              fontSize: 'large'
+            }}
+          >
+            {"Delete"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText
+              sx={{
+                font: 'unset',
+              }}
+              style={{
+                color: 'black',
+                fontSize: 'medium'
+              }}
+            >
+              Are you sure you want to delete this plan? This is irreversible
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions
+          >
+            <Button onClick={handleClose}
+              sx={{
+                backgroundColor: 'grey',
+                color: 'grey',
+                '&:hover':{
+                  backgroundColor: 'darkred'
+                },
+                textTransform: 'none'
+              }}
+              style={{
+                color: 'black', backgroundColor: 'lightgrey',
+              }}
+            >
+              Don't delete
+            </Button>
+            <Button onClick={()=>{handleDelete()}} autoFocus
+              sx={{
+                backgroundColor: 'rgb(248, 113, 113)',
+                color: 'black',
+                '&:hover':{
+                  backgroundColor: 'darkred'
+                },
+                textTransform: 'none',
+                
+                fontWeight: 'bold'
+              }}
+              style={{
+                color: 'white', backgroundColor: 'rgb(255, 113, 113)',
+                
+              }}
+            >
+              Yes, delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+    </div>
+  )
+}
+
+export default ProfileTripCard
---
+++ frontend/pages/profile.js
@@ -0,0 +1,182 @@
+import React, { useState, useEffect } from "react";
+import { useAuth } from "../hooks/auth";
+import { useRouter } from 'next/router';
+import Header from "../components/Header";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material'
+import ProfileTripCard from "../components/ProfileTripCard";
+
+const profile = ({ initialUser }) => {
+    const { cookies } = useAuth()
+    const [user, setUser] = useState(initialUser)
+    // const [isRefreshing, setIsRefreshing] = useState(false);
+    const [username, setUsername] = useState(initialUser.username)
+    const [email, setEmail] = useState(initialUser.email)
+	const [openEdit, setOpenEdit] = useState(false)
+	const [save, setSave] = useState(false)
+	const [trips, setTrips]  = useState([])
+
+	const router = useRouter()
+	console.log(trips)
+
+    useEffect(() => {
+		updateProfile()
+		setSave(false)
+    }, [save]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				let dev = process.env.NODE_ENV !== 'production';
+
+				const url = `${dev ? process.env.NEXT_PUBLIC_DEV_API_URL : process.env.NEXT_PUBLIC_PROD_API_URL}/user/trip/${cookies.id}`
+
+				const response = await fetch(url, {
+					method: 'GET',
+					headers: {
+					'Content-Type': 'application/json',
+					}
+				})
+
+				const data = await response.json();
+				setTrips(data)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+
+		fetchData()
+
+	}, [])
+
+    const updateProfile = async () => {
+		console.log("update")
+		let dev = process.env.NODE_ENV !== 'production';
+		const url = `${dev ? process.env.NEXT_PUBLIC_DEV_API_URL : process.env.NEXT_PUBLIC_PROD_API_URL}/user/${cookies.id}`
+		const response = await fetch(url, {
+			body: JSON.stringify(user),
+			method: 'PATCH',
+			headers: {
+			'Content-Type': 'application/json',
+			}
+		})
+		const data = await response.json()
+    }
+
+    const handleOpenEdit = () => {
+		setOpenEdit(true)
+    }
+
+    const handleCloseEdit = () => {
+		setOpenEdit(false)
+		setUsername(user.username)
+    }
+
+	const handleSaveEdit = async () => {
+		setOpenEdit(false)
+		setUser(prev => ({
+			...prev,
+			username: username
+		}))
+		setSave(true)
+	}
+
+    const handleOpenSettings = () => {
+
+    }
+    
+  return (
+    <div className='h-full bg-white'>
+        <Header/>
+        <div className='gap-4 h-screen bg-white flex flex-col items-center p-16'>
+			<h1 className="font-bold text-2xl mt-6 ">Profile</h1>
+            <section className="px-8 py-4 bg-white flex flex-row justify-between items-center gap-4 w-fit border-2 rounded-3xl">
+				<div className="flex flex-col mr-6">
+					<p className="font-bold">@{user.username}</p>
+					<p className="text-gray-600 text-sm italic">{user.email}</p>
+					<p className="font-bold mt-2">Trips:</p>
+					<p className="text-2xl">{user.trips.length}</p>
+              	</div>
+              	<div className="flex flex-col items-center justify-center gap-2 mt-4">
+					<button className='bg-gray-100 text-sm rounded-xl p-2 px-3 font-semibold hover:bg-gray-200' onClick={()=>{handleOpenEdit()}}>Edit</button>
+					<button className='bg-gray-100 text-sm rounded-xl p-2 px-3 font-semibold hover:bg-gray-200' onClick={()=>{handleOpenSettings()}}>Settings</button>
+              	</div>
+            </section>
+			<h1 className="font-bold text-xl">Your Trips</h1>
+            <section className="flex flex-col w-full h-fit border-2 rounded-xl items-start">
+				{trips ? 
+				(
+					trips.map((trip, index)=>(
+					<ProfileTripCard
+						key={trip._id}
+						index={index}
+						length={trips.length}
+						id={trip._id}
+						location={trip.location} 
+						startDate={trip.startDate} 
+						endDate={trip.endDate}
+						year={trip.startDate}
+						title={trip.title}
+					/>
+				))
+				):(
+					<p>You have no trips. Add a trip!</p>
+				)}
+            </section>
+			<Dialog open={openEdit} onClose={handleCloseEdit}>
+				<DialogTitle>Edit Profile</DialogTitle>
+				<DialogContent
+					sx={{
+						display: 'flex',
+						flexDirection: 'row',
+						alignItems: 'center'
+					}}
+				>
+				<p className='font-semibold mr-1 mt-2 inline py-1'>Username</p>
+				<input
+					className='bg-gray-200 mt-2 w-full rounded-lg py-1 px-2 text-gray-500 outline-none font-normal [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none'
+					type='text'
+					placeholder='Username'
+					value={username}
+					onChange={(e)=>{setUsername(e.target.value)}}
+				/>
+				</DialogContent>
+				<DialogActions>
+				<Button onClick={()=>{handleCloseEdit()}}>Cancel</Button>
+				<Button onClick={()=>{handleSaveEdit()}}>Save</Button>
+				</DialogActions>
+			</Dialog>
+        </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps(ctx) {
+    const cookies = ctx.req.cookies
+  try {
+    let dev = process.env.NODE_ENV !== 'production';
+    const url = `${dev ? process.env.NEXT_PUBLIC_DEV_API_URL : process.env.NEXT_PUBLIC_PROD_API_URL}/user/${cookies.id}`
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    const data = await response.json()
+
+    return {
+      props: {
+        initialUser: data,
+      }
+    }
+  } catch (error) {
+      console.log(error)
+      return {
+        props: {
+          trips: [],
+        }
+      }
+  }
+}
+
+export default profile
+
